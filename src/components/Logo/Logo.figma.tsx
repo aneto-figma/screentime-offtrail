8a9da705a193c0e45539f7014b1d87c11a61c3f5
lchoung-figma <lchoung@figma.com>
2026-06-10 10:36:18 -0400

    remove old code connect

--- src/components/Logo/Logo.figma.tsx
+++
@@ -1,9 +0,0 @@
-import figma from "@figma/code-connect"
-
-figma.connect(
-  "https://www.figma.com/design/9dQKfyAgC2IP6CDIKPdeFk/Offtrail?node-id=397-10123",
-  {
-    example: () => <Logo />,
-    imports: ["import Logo from '@/components/Logo'"],
-  }
-)
